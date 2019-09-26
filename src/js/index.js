9ff6063d2e25264d021d7eab9e33c03d5821f442
manpreet singh <manpreet.singh10196@gmail.com>
2019-09-26 21:22:54 +0530

    modified

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
 //Global app controller
 import num from './test';
 
-console.log(`I imported ${num} from another module!`);
+console.log(`I imported ${num} from another module called test.js!`);
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ const path= require('path');
 module.exports = { 
     entry:'./src/js/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist/js'),
-        filename:'bundle.js'
+        path: path.resolve(__dirname, 'dist'),
+        filename:'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
     }
 };
