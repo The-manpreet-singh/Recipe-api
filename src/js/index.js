a18f603a2213115c6bc3d074df584fc10d55446e
manpreet singh <manpreet.singh10196@gmail.com>
2019-10-03 21:07:32 +0530

    add models and view

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,18 @@
-//Global app controller
-import num from './test';
-const x=23;
-console.log(`I imported ${num} from another module called test.js variable x is ${x}`);
+import axios from 'axios';
+
+async function getResults(query){
+    const proxy='https://cors-anywhere.herokuapp.com/';
+    const key='f469cde0dbd53309ebe643d4993afb4d';
+    try{
+        const res= await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    }
+    catch(error){
+       alert(error);
+    }
+}
+getResults('tomato pasta');
+
+//https://www.food2fork.com/api/search
+//f469cde0dbd53309ebe643d4993afb4d
--- src/js/test.js
+++
@@ -1,2 +0,0 @@
-console.log('Imported module');
-export default 12345;
