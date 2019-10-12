eccc9616dfc8d8064b506a503b2d60907333e5f7
manpreet singh <manpreet.singh10196@gmail.com>
2019-10-12 15:42:15 +0530

    add spinner

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import {elements} from './views/base';
+import {elements, renderLoader, clearLoader} from './views/base';
 
 /* Global state of the app
  *- Search object
@@ -23,11 +23,13 @@ const controlSearch = async () =>{
       //3) Prepare UI for results
       searchView.clearInput(); 
       searchView.clearResults();
+      renderLoader(elements.searchRes);
       //4) Search for recipes
       await state.search.getResults();
 
       //5) render results on UI
       //console.log(state.search.result);
+      clearLoader(); 
       searchView.renderResults(state.search.result);
   }
 }
--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,5 +1,26 @@
 export const elements= {
       searchForm: document.querySelector('.search'),
       searchInput: document.querySelector('.search__field'),
+      searchRes: document.querySelector('.results'), 
       searchResList : document.querySelector('.results__list')
+};
+
+export const elementString= {
+      loader:'loader'
+};
+
+export const renderLoader = parent =>{
+     const loader = `
+      <div class="${elementString.loader}">
+            <svg>
+                 <use href="img/icons.svg#icon-cw"></use> 
+            </svg>
+      </div>
+      `;
+      parent.insertAdjacentHTML('afterbegin', loader);
+};
+
+export const clearLoader = () => {
+        const loader =document.querySelector(`.${elementString.loader}`);
+        if(loader) loader.parentElement.removeChild(loader); 
 };
